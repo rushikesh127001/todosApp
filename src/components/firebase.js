f230d4214b164e1f42eead1f981e48f1bcfec2a0
rushikesh.chaskar <rushikesh.chaskar@newfold.com>
2022-05-13 13:28:55 +0530

    updated colors

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -6,15 +6,6 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyC3Mld9DsxpH4GCsQUoD8lrf_0-ZYHbBbs",
-    authDomain: "todoapp-ef13d.firebaseapp.com",
-    projectId: "todoapp-ef13d",
-    storageBucket: "todoapp-ef13d.appspot.com",
-    messagingSenderId: "662550019268",
-    appId: "1:662550019268:web:7eaaea2db4521a528bdd91",
-    measurementId: "G-M7EB92M78J"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
